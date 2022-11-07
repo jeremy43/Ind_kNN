d1ffb1213113e317d126bd134f21e8227a2842a9
jeremy43 <yuqingzhu@ucsb.edu>
2022-11-07 10:54:02 -0800

    edit weight_knn

--- weight_knn.py
+++ weight_knn.py
@@ -7,7 +7,6 @@ import numpy as np
 import torch
 import network
 from torch.utils.data import DataLoader
-import aggregation
 import utils
 import sys
 import os
@@ -50,14 +49,30 @@ def PrepareData(dataset, feature, num_query):
         test_labels = test_dataset.targets
         train_labels = train_dataset.targets
         return train_data, train_labels, test_data, test_labels
+    
     if feature == 'resnet50':
         weight = ResNet50_Weights.IMAGENET1K_V2
         preprocess = weight.transforms()
         if dataset == 'cifar10':
-            train_dataset = datasets.CIFAR10(root=dataset_path, train=True, download=True,transform=transforms.Compose(
+            train_dataset = datasets.CIFAR10(root=dataset_path, train=True, download=True,  transform=transforms.Compose(
                                         [transforms.ToTensor(), preprocess]
                                     ))
-            test_dataset = datasets.CIFAR10(root=dataset_path, train=False, download=True,
+            test_dataset = datasets.CIFAR10(root=dataset_path, train=False, download=True, transform=transforms.Compose(
+                                           [transforms.ToTensor(), preprocess]
+                                       ))
+        elif dataset == 'INaturalist':
+            train_dataset = datasets.INaturalist(root=dataset_path, version='2021_train_mini',   transform=transforms.Compose([transforms.ToTensor(), preprocess]
+                                    ))
+            print('train_dataset', len(train_dataset))
+            test_dataset = train_dataset
+            #test_dataset = datasets.INaturalist(root=dataset_path, version='2021_valid',download=True,  transform=transforms.Compose(
+            #                               [transforms.ToTensor(), preprocess]
+            #                           ))
+        elif dataset == 'LSUN':
+            train_dataset = datasets.LSUN(root=dataset_path, classes='train', transform=transforms.Compose(
+                                        [transforms.ToTensor(), preprocess]
+                                    ))
+            test_dataset = datasets.LSUN(root=dataset_path, classes='test', 
                                        transform=transforms.Compose(
                                            [transforms.ToTensor(), preprocess]
                                        ))
@@ -65,7 +80,7 @@ def PrepareData(dataset, feature, num_query):
             train_dataset = datasets.CIFAR100(root=dataset_path, train=True, download=True,transform=transforms.Compose(
                                         [transforms.ToTensor(), preprocess]
                                     ))
-        test_dataset = datasets.CIFAR100(root=dataset_path, train=False, download=True,
+            test_dataset = datasets.CIFAR100(root=dataset_path, train=False, download=True,
                                        transform=transforms.Compose(
                                            [transforms.ToTensor(), preprocess]
                                        ))
@@ -83,13 +98,29 @@ def PrepareData(dataset, feature, num_query):
         
          
     train_data, test_data = extract_feature(train_dataset, test_dataset, feature, dataset)
-    test_labels = test_dataset.targets
-    train_labels = train_dataset.targets
+    idx = 0
+    if dataset == 'INaturalist':
+        train_labels = []
+        for (img, target) in train_dataset:
+            #print('target', target)
+            idx+=1
+            if idx%1000==0:
+                print(f"idx is {idx}")
+            train_labels.append(target)
+        test_labels = []
+        print('idx', idx)
+        for (img, target) in test_dataset:
+            test_labels.append(target)
+    else:
+        test_labels = test_dataset.targets
+        #print('test_labels', test_labels[:100])
+        train_labels = train_dataset.targets
     return train_data, train_labels, test_data[:num_query], test_labels[:num_query]
 
 
-def IndividualkNN(dataset, feature='resnet50', nb_teachers=150, num_query=1000, nb_labels=10, ind_budget=20, noisy_scale=0.1, var=1.):
+def IndividualkNN(dataset, feature='resnet50', nb_teachers=150, num_query=1000, nb_labels=10, ind_budget=20, noisy_scale=0.1, var=1., norm='L2'):
     # mask_idx masked private data that are deleted.  only train_data[mask_idx!=0] will be used for kNN.
+    print('which norm', norm)
     private_data_list, private_label_list, query_data_list, query_label_list = PrepareData(dataset, feature, num_query)
     print('shape of feature', private_data_list.shape)
     mask_idx = np.ones(len(private_data_list)) * ind_budget
@@ -109,18 +140,28 @@ def IndividualkNN(dataset, feature='resnet50', nb_teachers=150, num_query=1000, 
         filter_private_data  = private_data_list[mask_idx>0]
         # Implement no sampling strategy first
         #select_teacher = np.random.choice(private_data.shape[0], int(prob * num_train))
-        dis = np.linalg.norm(filter_private_data - query_data, axis=1)
+        if norm == 'L2':
+            dis = np.linalg.norm(filter_private_data - query_data, axis=1)
+        else:
+            # compute cos similarity 
+            dis = [np.dot(x, query_data)/(np.linalg.norm(x)*np.linalg.norm(query_data)) for x in filter_private_data]
+            dis = -np.array(dis)
+            #dis = -np.dot(filter_private_data, query_data)/(np.linalg.norm(filter_private_data, axis=1)*np.linalg.norm(query_data))
         keep_idx = original_idx[np.where(mask_idx > 0)[0]]
         #print(f"argsort={keep_idx}")
         original_topk_index_set = keep_idx[np.argsort(dis)[:nb_teachers]]
         #print(f"original_topk_index_set={original_topk_index_set}")
         # For each data in original_tok_index, update their individual accountant.
-        kernel_weight = [np.exp(-np.linalg.norm(private_data_list[i]-query_data)**2/var) for i in original_topk_index_set]
+        if norm == 'L2':
+            kernel_weight = [np.exp(-np.linalg.norm(private_data_list[i]-query_data)**2/var) for i in original_topk_index_set]
+        else:
+            kernel_weight = [np.dot(private_data_list[i], query_data)/(np.linalg.norm(private_data_list[i])*np.linalg.norm(query_data)) for i in original_topk_index_set]
+        #print('kernel weight', kernel_weight[:10])
         #copy_kernel_weight = [np.exp(-dis[i]**2/var) for i in np.argsort(dis)[:nb_teachers]]
         kernel_weight = np.asarray(kernel_weight)
         sum_kernel_weight = sum(kernel_weight)
         #print('sum_kernel_weight', sum_kernel_weight)
-        normalized_weight = kernel_weight/ sum_kernel_weight * nb_teachers
+        normalized_weight = kernel_weight#/ sum_kernel_weight * nb_teachers
         vote_count = np.zeros(nb_labels)
         #print('normalized_weight', normalized_weight)
         #print('vote_count', vote_count)
@@ -170,6 +211,7 @@ if __name__ =='__main__':
     parser.add_argument('--nb_labels', type=int, default=10)
     parser.add_argument('--num_query', type=int, default=1000)
     parser.add_argument('--var', type=float, default=8.)
+    parser.add_argument('--norm', choices=['L2', 'cos'], default='L2')
     args = parser.parse_args()
     ac_labels  = IndividualkNN(**vars(args))
     #return ac_labels
