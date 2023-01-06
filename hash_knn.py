a24aa73db99888f36e78fc8c1eba3a0b7fef20b3
jeremy43 <yuqingzhu@ucsb.edu>
2023-01-05 20:31:38 -0800

    hash

--- hash_knn.py
+++ hash_knn.py
@@ -10,6 +10,7 @@ from datasets import load_dataset
 import torch
 import network
 from torch.utils.data import DataLoader
+from correct_weight_knn import PrepareData
 from sentence_transformers import util
 import utils
 import sys
@@ -21,118 +22,12 @@ from utils import extract_feature
 dataset_path = '/home/yq/dataset'
 
 
-def PrepareData(dataset, feature, num_query, dataset_path):
-    """
-    Takes a dataset name and the size of the teacher ensemble and prepares
-    training data for the student model, according to parameters indicated
-    in flags above.
-    :param dataset: string corresponding to mnist, cifar10, or svhn
-    :param nb_teachers: number of teachers (in the ensemble) to learn from
-    :param save: if set to True, will dump student training labels predicted by
-                 the ensemble of teachers (with Laplacian noise) as npy files.
-                 It also dumps the clean votes for each class (without noise) and
-                 the labels assigned by teachers
-    :return: pairs of (data, labels) to be used for student training and testing
-
-    """
-
-    # Load the dataset
-    if dataset == 'mnist':
-        normalize = transforms.Normalize(mean=[0.485], std=[0.22])
-
-        train_dataset = datasets.MNIST(root=dataset_path, train=False, download=True,
-                                    transform=transforms.Compose(
-                                        [transforms.ToTensor(), normalize]
-                                    ))
-        test_dataset = datasets.MNIST(root=dataset_path, train=False, download=True,
-                                     transform=transforms.Compose(
-                                        [transforms.ToTensor(), normalize]
-                                    ))
-        scattering, K, (h, w) = utils.get_scatter_transform()
-        train_data, test_data = extract_feature(train_dataset, test_dataset)
-        test_labels = test_dataset.targets
-        train_labels = train_dataset.targets
-        return train_data, train_labels, test_data, test_labels
-    
-    if feature == 'resnet50':
-        weight = ResNet50_Weights.IMAGENET1K_V2
-        preprocess = weight.transforms()
-        if dataset == 'cifar10':
-            train_dataset = datasets.CIFAR10(root=dataset_path, train=True, download=True,  transform=transforms.Compose(
-                                        [transforms.ToTensor(), preprocess]
-                                    ))
-            test_dataset = datasets.CIFAR10(root=dataset_path, train=False, download=True, transform=transforms.Compose(
-                                           [transforms.ToTensor(), preprocess]
-                                       ))
-            test_labels = test_dataset.targets
-            train_labels = train_dataset.targets
-        elif dataset == 'INaturalist':
-            train_dataset = datasets.INaturalist(root=dataset_path, version='2021_train_mini',  transform=transforms.Compose([transforms.ToTensor(), preprocess]
-                                    ))
-            test_dataset = datasets.INaturalist(root=dataset_path+'/val', version='2021_valid', transform=transforms.Compose(
-                
-                                           [transforms.ToTensor(), preprocess]
-                                       ))
-            train_labels = utils.extract_label(train_dataset, 'train_mini')
-            test_labels = utils.extract_label(test_dataset, 'val')
-
-        elif dataset == 'LSUN':
-            train_dataset = datasets.LSUN(root=dataset_path, classes='train', transform=transforms.Compose(
-                                        [transforms.ToTensor(), preprocess]
-                                    ))
-            test_dataset = datasets.LSUN(root=dataset_path, classes='test', 
-                                       transform=transforms.Compose(
-                                           [transforms.ToTensor(), preprocess]
-                                       ))
-        elif dataset == 'cifar100':
-            train_dataset = datasets.CIFAR100(root=dataset_path, train=True, download=True,transform=transforms.Compose(
-                                        [transforms.ToTensor(), preprocess]
-                                    ))
-            test_dataset = datasets.CIFAR100(root=dataset_path, train=False, download=True,
-                                       transform=transforms.Compose(
-                                           [transforms.ToTensor(), preprocess]
-                                       ))
-    elif feature == 'resnext29':
-        normalize = transforms.Normalize(mean=[0.491, 0.482, 0.4465],
-                                         std=[0.202, 0.1994, 0.2010])
-        
-        train_dataset = datasets.CIFAR10(root=dataset_path, train=True, download=True,transform=transforms.Compose(
-                                        [transforms.ToTensor(), normalize]
-                                    ))
-        test_dataset = datasets.CIFAR10(root=dataset_path, train=False, download=True,
-                                       transform=transforms.Compose(
-                                           [transforms.ToTensor(), normalize]
-                                       ))
-        test_labels = test_dataset.targets
-        train_labels = train_dataset.targets
-    elif feature == 'all-roberta-large-v1':
-        if dataset == 'sst2':
-            ori_dataset = load_dataset('glue', 'sst2')
-            train_dataset = ori_dataset['train']['sentence']
-            test_dataset = ori_dataset['test']['sentence']
-            train_labels = ori_dataset['train']['label']
-            #test_labels = ori_dataset['validation']['label']
-            test_labels = ori_dataset['test']['label']
-        elif dataset == 'agnews':
-            ori_dataset = load_dataset('ag_news')
-            train_dataset = ori_dataset['train']['text']
-            test_dataset = ori_dataset['test']['text']
-            train_labels = ori_dataset['train']['label']
-            test_labels = ori_dataset['test']['label']
-    train_data, test_data = extract_feature(train_dataset, test_dataset, feature, dataset) 
-    train_labels = np.array(train_labels)
-    test_labels = np.array(test_labels)
-    train_data = np.array(train_data)
-    np.random.seed(0)
-    print(f'length of test data {test_data.shape[0]}')
-    random_index = np.random.randint(0, test_data.shape[0], num_query).astype(int)
-    return train_data, train_labels, test_data[random_index], test_labels[random_index]
 
 
 def IndividualkNN(dataset,  hash_method = 'knn', min_weight = 0.2,  num_tables = 2, proj_dim=12,feature='resnet50',clip=0.25, nb_teachers= 50, num_query=1000, nb_labels=10, ind_budget=20, noisy_scale=0.1, var=1., norm='L2', dataset_path=None):
     # mask_idx masked private data that are deleted.  only train_data[mask_idx!=0] will be used for kNN.
     print('which norm', norm)
-    private_data_list, private_label_list, query_data_list, query_label_list = PrepareData(dataset, feature, num_query, dataset_path)
+    private_data_list, private_label_list, query_data_list, query_label_list = PrepareData(dataset, feature, num_query, dataset_path, norm='centering+L2')
     print('shape of feature', private_data_list.shape)
     # construct hash table
     print(f'num_tables is {num_tables} and proj_dim is {proj_dim}')
@@ -169,7 +64,7 @@ def IndividualkNN(dataset,  hash_method = 'knn', min_weight = 0.2,  num_tables =
         #print(f"argsort={keep_idx}")
         select_neighbors = [x for x in hash_neighbors if mask_idx[x]>0]
         select_neighbors = np.array(select_neighbors, dtype=int)
-        sum_neighbors+=len(select_neighbors)
+        #sum_neighbors+=len(select_neighbors)
         vote_count = np.zeros(nb_labels)
         #select_neighbors = np.array(select_neighbors)
         #temp_d = util.cos_sim(private_data_list[select_neighbors], query_data).reshape(-1)
@@ -214,16 +109,17 @@ def IndividualkNN(dataset,  hash_method = 'knn', min_weight = 0.2,  num_tables =
                 neighbor = select_neighbors[i]
                 mask_idx[neighbor]-=  normalized_weight[i]
         elif hash_method =='basic+threshold':
-            select_neighbor = select_neighbor[np.where(kernel_weight>min_weight)[0]]
+            keep_idx = np.where(kernel_weight>min_weight)[0]
+            select_neighbors = select_neighbors[keep_idx]
             for i in range(len(select_neighbors)):
                 neighbor  = select_neighbors[i]
-                vote_count[private_label_list[neighbor]]+= min(mask_idx[neighbor],normalized_weight[i])
-                mask_idx[neighbor]-=  normalized_weight[i]
+                vote_count[private_label_list[neighbor]]+= min(mask_idx[neighbor],normalized_weight[keep_idx[i]])
+                mask_idx[neighbor]-=  normalized_weight[keep_idx[i]]
         else:
             print('wrong approach')
-        # print(f'max vote_count is{max(vote_count)} sum vote count is {sum(vote_count)}')
+        #print(f'max vote_count is{max(vote_count)} sum vote count is {sum(vote_count)}')
         
-        
+        sum_neighbors +=len(select_neighbors)
         for i in range(nb_labels):
             vote_count[i]+=np.random.normal(scale=noisy_scale)
         
--- run_hash_db.py
+++ run_hash_db.py
@@ -1,10 +1,10 @@
 from hash_knn import IndividualkNN
 import numpy as np
-
-NUM_QUERY = 300
+import pickle
+NUM_QUERY = 500
 VARS  = np.exp([1.0])
-NB_HASH_TABLES =  [12, 16, 20]
-PROJ_DIMS =  [12]
+NB_HASH_TABLES =  [ 20]
+PROJ_DIMS =  [12, 10, 9]
 FEATURE = 'resnet50'
 CLIPS=[0.25]
 FEATURE = 'all-roberta-large-v1'
@@ -15,13 +15,15 @@ DATASET_PATH = '/home/yq/dataset'
 EPS_LIST = [1.3**x*0.1 for x in range(12)]
 NOISE_MUL_LIST = [30.75, 24.19, 19.03, 14.96, 11.76, 9.24, 7.26, 5.71, 4.49, 3.54, 2.79, 2.2]
 IND_BUDGETS =np.exp([-20, -18, -16, -14])
-IND_BUDGETS =np.exp([-1])
+IND_BUDGETS =np.exp([-1.3, -1.4])
 NORM = 'L2'
 num_point = 12
 idx = 0
-hash_method = 'basic+threshold'
+hash_method = 'knn'
+hash_file_name = f'hash_{DATASET}_method_{hash_method}_Query_{NUM_QUERY}_record.pkl'
+print(f'file_name is {hash_file_name}')
 test_ac_list = []
-for idx  in range(8, 9):
+for idx  in range(10, 12):
     eps = EPS_LIST[idx]
     print('idx ', idx, 'current epsilon', eps)
     noise_mul  = NOISE_MUL_LIST[idx]
@@ -34,8 +36,9 @@ for idx  in range(8, 9):
             print('current budget is', ind_budget, 'sigma is', sigma)
             #clip = 0.5
             #clip = ind_budget
-            for min_weight  in [ 0.6]:
-            #for nb_teachers in [50, 100, 200, 1000]:
+            #for min_weight  in [ 0.6]:
+            for nb_teachers in [50, 100]:
+                min_weight = 0.1
                 clip = 0.25
                 nb_teachers = 20
                 #for max_dis in np.exp([2.5, 2.75] ):
@@ -47,3 +50,13 @@ for idx  in range(8, 9):
                  noisy_scale=sigma, clip=clip, min_weight = min_weight,  nb_teachers = nb_teachers, hash_method=hash_method,  feature=FEATURE, num_query=NUM_QUERY,  num_tables=nb_tables, proj_dim=proj_dim,
                  ind_budget=ind_budget, nb_labels=14, norm='L2', dataset_path=DATASET_PATH)
                         print(f"accuracy={ac}")
+    optimal_ac = max(optimal_ac, ac)
+    print(f'when eps is{eps} the best accuracy is {optimal_ac}')
+    test_ac_list.append(optimal_ac)
+
+record = {}
+record['eps_list'] = EPS_LIST
+record['noise_mul_list'] = NOISE_MUL_LIST
+record['acc_kNN'] = test_ac_list
+with open(hash_file_name, 'wb') as f:
+    pickle.dump(record, f)
