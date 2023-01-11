30ceffa64e1a0ea246fab89632eccc3f4d89c058
jeremy43 <yuqingzhu@ucsb.edu>
2023-01-11 14:10:09 -0800

    add rescale knn

---
+++ rescale_weight_knn.py
@@ -0,0 +1,142 @@
+from torchvision import datasets, transforms
+from torchvision.models import resnet50, ResNet50_Weights
+from torchvision import datasets as dataset
+from datasets import load_dataset
+import numpy as np
+import torch
+import network
+from torch.utils.data import DataLoader
+import utils
+import sys
+import metrics
+import argparse
+from utils import extract_feature
+from sentence_transformers import util
+from correct_weight_knn import PrepareData
+dataset_path = '/home/yq/dataset'
+
+
+def IndividualkNN(dataset, kernel_method='rbf', feature='resnet50', nb_teachers=150, num_query=1000,  nb_labels=10, ind_budget=20,min_weight=0.1,  noisy_scale=0.1, sigma_1 = 0.1,  seed=0, var=1., norm='centering+L2',dataset_path=None):
+    # mask_idx masked private data that are deleted.  only train_data[mask_idx!=0] will be used for kNN.
+    private_data_list, private_label_list, query_data_list, query_label_list = PrepareData(dataset, feature, num_query, dataset_path,seed, norm=norm)
+    print(f'length of query list={len(query_data_list)}')
+    print('shape of feature', private_data_list.shape)
+    mask_idx = np.ones(len(private_data_list)) * ind_budget
+    # pointer to original idx.
+    original_idx = np.array([x for x in range(len(private_data_list))])
+    # keep_idx denote the data that is not deleted.
+    max_clip = 0
+    sum_neighbors = 0
+    teachers_preds = np.zeros([num_query, nb_teachers])
+    predict_labels = []
+    track_k_weight = []
+    num_data = []
+    for idx in range(num_query):
+        query_data = query_data_list[idx]
+        if idx % 100 == 0:
+            print('current query idx', idx)
+        # print(f'idx is {idx}')
+        filter_private_data = private_data_list[mask_idx > 0]
+        
+        if kernel_method=='cosine':
+            dis =  -np.dot(filter_private_data, query_data)
+            #dis = -util.cos_sim(filter_private_data, query_data).reshape(-1)
+        elif kernel_method=='RBF' or 'student':
+            dis = np.linalg.norm(filter_private_data - query_data, axis=1)
+        keep_idx = original_idx[np.where(mask_idx > 0)[0]]
+        # to speed up the experiment, only keep the top 3k neighbors' prediction.
+        num_data.append(len(keep_idx))
+        #print(f'number of data is {len(keep_idx)}')
+        keep_idx =keep_idx[np.argsort(dis)[:5000]] 
+        #print(f'length of keep_idx is {len(keep_idx)}')
+        if len(keep_idx)==0 or len(keep_idx)==1:
+            print('private dataset is now empty')
+            predict_labels.append(0)
+            continue
+        if kernel_method=='cosine':
+            kernel_weight = np.dot(private_data_list[keep_idx], query_data)
+            #temp_d = util.cos_sim(private_data_list[keep_idx], query_data).reshape(-1)
+            #kernel_weight = [np.exp(-temp_d[i] ** 2 / var) for i in range(len(keep_idx)) ]
+        elif kernel_method=='RBF':
+            kernel_weight = [np.exp(-np.linalg.norm(private_data_list[x] - query_data) ** 2 / var) for x in keep_idx]
+        # print(f'length of keep_idx is {len(keep_idx)}')
+        elif kernel_method == 'student':
+             kernel_weight = [(1+np.linalg.norm(private_data_list[i] - query_data) ** 2 / var)**(-(var+1)/2) for i in keep_idx]
+        if len(keep_idx) == 0 or len(keep_idx) == 1:
+            print('private dataset is empty')
+            predict_labels.append(0)
+            continue
+        kernel_weight = np.array(kernel_weight)
+        #normalized_weight = [x*min(1, clip/x) for x in kernel_weight]
+        #track_k_weight.append(kernel_weight[10])
+        #print(f' min of weight is {min(kernel_weight)} and max weight is {max(kernel_weight)}')
+        #normalized_weight = [x/sum_weight for x in kernel_weight]
+        normalized_weight = np.array(kernel_weight)
+        keep_idx_in_normalized = np.where(normalized_weight > min_weight)[0]
+        n_neighbor = len(keep_idx_in_normalized)
+        n_neighbor = max(n_neighbor + np.random.normal(scale=sigma_1), 30)
+        print('number of neighbor', n_neighbor, 'true num of neighbor', len(keep_idx_in_normalized))
+        rescale_noise = np.sqrt(n_neighbor) * noisy_scale
+        # cur_weight = min_weight
+        # while len(keep_idx_in_normalized) < 100 and cur_weight > 0:
+        #     cur_weight -= 0.1
+        #     keep_idx_in_normalized = np.where(normalized_weight > cur_weight)[0]
+        # print(f'keep idx ={len(keep_idx_in_normalized)}')
+        original_top_index_set = keep_idx[keep_idx_in_normalized]
+        # print(f'length of neighbors is {len(original_top_index_set)}')
+        sum_neighbors += len(original_top_index_set)
+        # original_top_index_set =  keep_idx[np.where(kernel_weight>min_weight)]
+        vote_count = np.zeros(nb_labels)
+        if len(original_top_index_set) == 0:
+            predict_labels.append(0)
+            continue
+        # count privacy loss: loss of releasing num of neighbor and the loss of releasing kernel weight
+        for i in range(len(original_top_index_set)):
+            select_idx = original_top_index_set[i]
+            idx_normalized = keep_idx_in_normalized[i]
+            n_neighbor_contrib = 1.0/(2*sigma_1**2)
+            rescale_contrib = normalized_weight[idx_normalized]**2/(2*rescale_noise**2)
+            mask_idx[select_idx]-=n_neighbor_contrib
+            if mask_idx[select_idx]<=0:
+                continue
+            vote_count[private_label_list[select_idx]] += min(np.sqrt(2*mask_idx[select_idx]*(rescale_noise**2)), normalized_weight[idx_normalized])
+            mask_idx[select_idx] -= rescale_contrib
+        #print(f'max vote count is {max(vote_count)} and noise scale is {noisy_scale}')
+        for i in range(nb_labels):
+            vote_count[i] += np.random.normal(scale=rescale_noise)
+        predict_labels.append(np.argmax(vote_count))
+    print('remain dataset size is', num_data[-1])
+    print('averaged neighbors is {}'.format(sum_neighbors / len(teachers_preds)))
+    print('answer {} queries over {}'.format(len(predict_labels), len(teachers_preds)))
+    # acct.compose_poisson_subsampled_mechanisms(gaussian2, prob,coeff = len(stdnt_labels))
+    predict_labels = np.array(predict_labels)
+    track_k_weight = np.array(track_k_weight)
+    print(f'average top k neighbor weight is {np.mean(track_k_weight)}')
+    accuracy = metrics.accuracy(predict_labels, query_label_list)
+    return accuracy * 100
+
+
+if __name__ == '__main__':
+    """
+    This function trains a student using predictions made by an ensemble of
+    teachers. The student and teacher models are trained using the same
+    neural network architecture.
+    :param dataset: string corresponding to celeba
+    :param nb_teachers: number of teachers (in the ensemble) to learn from
+    :return: True if student training went well
+    """
+    # Call helper function to prepare student data using teacher predictions
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', choices=['cifar10'], default='cifar10')
+    parser.add_argument('--feature', choices=['resnet50', 'resnext29'], default='resnet50')
+    parser.add_argument('--noisy_scale', type=float, default=0.1)
+    parser.add_argument('--ind_budget', type=float, default=20.)
+    parser.add_argument('--nb_teachers', type=int, default=150)
+    parser.add_argument('--nb_labels', type=int, default=10)
+    parser.add_argument('--num_query', type=int, default=1000)
+    parser.add_argument('--var', type=float, default=8.)
+    parser.add_argument('--norm', choices=['L2', 'cos'], default='L2')
+    parser.add_argument('--dataset_path', type=str, default='./dataset/')
+    args = parser.parse_args()
+    ac_labels = IndividualkNN(**vars(args))
+    # return ac_labels
---
+++ run_rescale_cifar10.py
@@ -0,0 +1,102 @@
+from rescale_weight_knn import IndividualkNN
+from hand_dp import hand_dp
+import numpy as np
+import random
+import pickle
+"""
+Set the Parameters
+"""
+# dataset_path = '/home/yq/dataset'
+# dataset_path = '/home/xuandong/mnt/dataset/'
+DATASET_PATH = '/home/yq/dataset'
+NB_TEACHERS = [100]
+NUM_CLASS = 10
+NUM_QUERY = 100
+VARS = np.exp([  1.7])
+CLIPS=[ 1.0]
+#NOISY_SCALES = [0]  # nondp
+FEATURE = 'resnet50'
+#DATASET = 'INaturalist'
+#FEATURE = 'all-roberta-large-v1'
+#DATASET = 'sst2'
+DATASET = 'cifar10'
+EPS_LIST = [1.3**x*0.1 for x in range(12)]
+NOISE_MUL_LIST = [30.75, 24.19, 19.03, 14.96, 11.76, 9.24, 7.26, 5.71, 4.49, 3.54, 2.79, 2.2]
+NORM = 'L2'
+num_point = 12
+
+SIGMA_KERNEL = [  4.0]
+kNN_file_name = f'kNN_{DATASET}_Query_{NUM_QUERY}_record.pkl'
+print(f'file_name is {kNN_file_name}')
+idx = 0
+kernel_method = 'RBF'
+test_ac_list = []
+h = 0.25 # ratio of budget for gaussian mechanism
+# sigma_1 = np.sqrt(T *noise_mul**2/h)
+seed = random.randint(1,100)
+for idx  in range(1):
+    eps = EPS_LIST[idx]
+    print('idx ', idx, 'current epsilon', eps)
+    noise_mul  = NOISE_MUL_LIST[idx]
+    optimal_ac = 0
+    # alpha * ind_budget  = alpha/(2*noise_mul**2)
+    ind_budget = 1.0/(2*noise_mul**2)
+    for basic_sigma in SIGMA_KERNEL:
+        #sigma = 0
+        #basic_sigma = 0
+        print('current budget is', ind_budget, 'sigma is', basic_sigma)
+        for h in [ 0.1, 0.5]:
+            # h represents the ratio of budget used to release the number of neighbors
+            sigma_1 = np.sqrt(NUM_QUERY/(2*ind_budget*h))
+            for var in VARS:
+                #for max_dis in np.exp([2.5, 2.75] ):
+                for min_weight in ([0.8]):
+                    each_ac_list = []
+                    for repeat in range(1):
+                        print(f"dataset={DATASET}, kernel_method={kernel_method}, min_weight={min_weight}, var={np.log(var)}, sigma_1 is {sigma_1},  basic_sigma={np.log(basic_sigma)}, ind_budget={ind_budget},  norm={NORM}")
+
+                        ac = IndividualkNN(dataset=DATASET, var=var, kernel_method=kernel_method, noisy_scale=basic_sigma,
+                                   feature=FEATURE, num_query=NUM_QUERY, min_weight=min_weight,  sigma_1 = sigma_1, 
+                                   ind_budget=ind_budget, nb_labels=NUM_CLASS,seed=seed, dataset_path=DATASET_PATH)
+                        print(f"accuracy={ac} \n")
+                        each_ac_list.append(ac)
+                each_ac_list = np.array(each_ac_list)
+                optimal_ac = max(optimal_ac, np.mean(each_ac_list))
+    print(f'when eps is{eps} the best accuracy is {optimal_ac}')
+    test_ac_list.append(optimal_ac)
+record = {}
+record['eps_list'] = EPS_LIST
+record['noise_mul_list'] = NOISE_MUL_LIST
+record['acc_kNN'] = test_ac_list
+with open(kNN_file_name, 'wb') as f:
+    pickle.dump(record, f)
+#print('indKNN acc', ac)
+"""
+#Result of handcrafted dp
+# such that the eps is chosen from [1.3**x*0.1 for x in range(10)]
+
+EPS_LIST = [1.3**x*0.1 for x in range(12)]
+SIGMA_LIST =  [5.5, 4.4, 3.5, 2.76, 2.2, 1.8, 1.53, 1.35, 1.21, 1.11, 1.02, 0.96]
+test_acc_list = []
+for (mul, eps) in zip(SIGMA_LIST, EPS_LIST):
+    acc = []
+    for repeat in range(5):
+        hand_acc = hand_dp(feature=FEATURE, batch_size=256, mini_batch_size=256,
+                   lr=10, optim="SGD", momentum=0.9, nesterov=False, noise_multiplier=mul,
+                   max_grad_norm=0.1, max_epsilon=None, epochs=10, logdir=None,
+                   dataset=DATASET, dataset_path=DATASET_PATH, num_query=NUM_QUERY, num_class=NUM_CLASS)
+    
+        acc.append(hand_acc)
+    acc = np.array(acc)
+    print('noisy multiplier is ', mul, ' mean of Hand DP acc', np.mean(hand_acc))
+    test_acc_list.append(acc)
+test_acc_list = np.array(test_acc_list)
+file_name = f'noisySGD_{DATASET}_Query_{NUM_QUERY}_record.pkl'
+record = {}
+record['eps_list'] = EPS_LIST
+record['sigma_list_noisySGD'] = SIGMA_LIST
+record['acc_noisySGD'] = test_acc_list
+with open(file_name, 'wb') as f:
+    pickle.dump(record, f)
+#print('indKNN acc', ac)
+"""
