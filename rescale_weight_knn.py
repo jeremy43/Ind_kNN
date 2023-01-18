cae5ec13c72935567a27dfe4fda9ae8b20d920a2
EC2 Default User <ec2-user@ip-172-31-24-57.us-east-2.compute.internal>
2023-01-18 00:39:40 +0000

    add opt in public

--- rescale_weight_knn.py
+++ rescale_weight_knn.py
@@ -15,12 +15,14 @@ from sentence_transformers import util
 from correct_weight_knn import PrepareData
 
 
-def IndividualkNN(dataset, kernel_method='rbf', feature='resnet50', nb_teachers=150, num_query=1000, nb_labels=10, ind_budget=20, min_weight=0.1, noisy_scale=0.1, sigma_1=0.1, seed=0, var=1., norm='centering+L2', dataset_path=None):
+def IndividualkNN(dataset, kernel_method='rbf', feature='resnet50', nb_teachers=150, num_query=1000, nb_labels=10, ind_budget=20, min_weight=0.1, noisy_scale=0.1, sigma_1=0.1, seed=0, var=1., norm='centering+L2', dataset_path=None, opt_public=True):
     # mask_idx masked private data that are deleted.  only train_data[mask_idx!=0] will be used for kNN.
     private_data_list, private_label_list, query_data_list, query_label_list = PrepareData(dataset, feature, num_query, dataset_path, seed, norm=norm)
     print(f'length of query list={len(query_data_list)}')
     print('shape of feature', private_data_list.shape)
     mask_idx = np.ones(len(private_data_list)) * ind_budget
+    public_mask = np.zeros(len(query_data_list))
+
     # pointer to original idx.
     original_idx = np.array([x for x in range(len(private_data_list))])
     # keep_idx denote the data that is not deleted.
@@ -33,11 +35,11 @@ def IndividualkNN(dataset, kernel_method='rbf', feature='resnet50', nb_teachers=
         if idx % 100 == 0:
             print('current query idx', idx)
         filter_private_data = private_data_list[mask_idx > 0]
-
         if kernel_method == 'cosine':
             dis = -np.dot(filter_private_data, query_data)
         elif kernel_method == 'RBF' or 'student':
             dis = np.linalg.norm(filter_private_data - query_data, axis=1)
+            
         keep_idx = original_idx[np.where(mask_idx > 0)[0]]
         # to speed up the experiment, only keep the top 3k neighbors' prediction.
         num_data.append(len(keep_idx))
@@ -49,8 +51,10 @@ def IndividualkNN(dataset, kernel_method='rbf', feature='resnet50', nb_teachers=
             continue
         if kernel_method == 'cosine':
             kernel_weight = np.dot(private_data_list[keep_idx], query_data)
+            public_kernel_weight = np.dot(query_data_list, query_data)
         elif kernel_method == 'RBF':
             kernel_weight = [np.exp(-np.linalg.norm(private_data_list[x] - query_data) ** 2 / var) for x in keep_idx]
+            public_kernel_weight = [np.exp(-np.linalg.norm(query_data_list[x] - query_data) ** 2 / var) for x in range(num_query)]
         elif kernel_method == 'student':
             kernel_weight = [(1 + np.linalg.norm(private_data_list[i] - query_data) ** 2 / var) ** (-(var + 1) / 2) for i in keep_idx]
         if len(keep_idx) == 0 or len(keep_idx) == 1:
@@ -60,6 +64,7 @@ def IndividualkNN(dataset, kernel_method='rbf', feature='resnet50', nb_teachers=
         kernel_weight = np.array(kernel_weight)
         # print(f' min of weight is {min(kernel_weight)} and max weight is {max(kernel_weight)}')
         normalized_weight = np.array(kernel_weight)
+        public_kernel_weight = np.array(public_kernel_weight)
         keep_idx_in_normalized = np.where(normalized_weight > min_weight)[0]
         n_neighbor = len(keep_idx_in_normalized)
         n_neighbor = max(n_neighbor + np.random.normal(scale=sigma_1), 30)
@@ -83,8 +88,16 @@ def IndividualkNN(dataset, kernel_method='rbf', feature='resnet50', nb_teachers=
             vote_count[private_label_list[select_idx]] += min(np.sqrt(2 * mask_idx[select_idx] * (rescale_noise ** 2)), normalized_weight[idx_normalized])
             mask_idx[select_idx] -= rescale_contrib
         # print(f'max vote count is {max(vote_count)} and noise scale is {noisy_scale}')
+        
+        if opt_public is True:
+            keep_idx_in_public = np.where((public_mask>0) & (public_kernel_weight>min_weight))[0]
+            print(f'using {len(keep_idx_in_public)} public data for prediction') 
+            for i in keep_idx_in_public:
+                vote_count[predict_labels[i]]+=public_kernel_weight[i]
+                
         for i in range(nb_labels):
             vote_count[i] += np.random.normal(scale=rescale_noise)
+        public_mask[idx] = 1000
         predict_labels.append(np.argmax(vote_count))
         if idx%200==0 and idx>0:
             tmp_predict_labels = np.array(predict_labels)
